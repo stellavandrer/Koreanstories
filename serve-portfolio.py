f51c2b5a1c7f9d6ea6e20d3869cd87920eb9d03d
stellavandrer <justinetilleul27@gmail.com>
2026-06-11 08:32:47 +0200

    Retire serve-portfolio.py (script local sans rapport avec le site)

--- serve-portfolio.py
+++
@@ -1,4 +0,0 @@
-import http.server, os
-os.chdir('/Users/justinetilleul/Desktop/DesignbyStelva/Site Web/Site')
-httpd = http.server.HTTPServer(('', 8766), http.server.SimpleHTTPRequestHandler)
-httpd.serve_forever()
